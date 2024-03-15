528457231d159a3c6eb39ed1121073fe58150c1a
Sumedh Bansod <162736736+Sumedhbansod@users.noreply.github.com>
2024-03-15 22:41:09 +0530

    script _4.sh
    commom command list

---
+++ script _4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -n " the current user : "
+whoami
+echo -n " present working directory "
+pwd
+echo -n " the current terminal status "
+who
+echo -n " list of files and directories "
+ls -l
